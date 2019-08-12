ec85a48d83154a6053a53c83ecc4eed4ac84ed5f
Wolven531 <anthony.williams.cs@gmail.com>
2019-08-11 22:48:29 -0600

    Add children items

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -8,10 +8,22 @@ const Grid: FC = () => {
 		<div className="grid" style={{
 			borderColor: '#fff',
 			display: 'grid',
+			gridAutoColumns: '33%',
+			gridAutoFlow: 'row',
+			gridAutoRows: '33%',
+			gridRowGap: '5px',
 			height: '300px',
-			width: '300px',
+			width: '100vw',
 		}}>
-			&nbsp;
+			<div>child 1</div>
+			<div>child 2</div>
+			<div>child 3</div>
+			<div>child 4</div>
+			<div>child 5</div>
+			<div>child 6</div>
+			<div>child 7</div>
+			<div>child 8</div>
+			<div>child 9</div>
 		</div>
 	)
 }
