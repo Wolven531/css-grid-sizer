a42a03e468230bc46dd6a8f93d8af34409247103
Wolven531 <anthony.williams.cs@gmail.com>
2019-08-11 22:40:39 -0600

    Introduce Grid component

---
+++ src/components/Grid/Grid.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from 'react'
+// import logo from '../../logo.svg'
+
+// import './Grid.css'
+
+const Grid: FC = () => {
+	return (
+		<div className="grid" style={{
+			borderColor: '#fff',
+			display: 'grid',
+			height: '300px',
+			width: '300px',
+		}}>
+			&nbsp;
+		</div>
+	)
+}
+
+export { Grid }
