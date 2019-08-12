0bf945240ae6f5ac782a619037239cfcfa8eb651
Wolven531 <anthony.williams.cs@gmail.com>
2019-08-11 22:49:25 -0600

    Add class names to children

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -15,15 +15,15 @@ const Grid: FC = () => {
 			height: '300px',
 			width: '100vw',
 		}}>
-			<div>child 1</div>
-			<div>child 2</div>
-			<div>child 3</div>
-			<div>child 4</div>
-			<div>child 5</div>
-			<div>child 6</div>
-			<div>child 7</div>
-			<div>child 8</div>
-			<div>child 9</div>
+			<div className="child child1">child 1</div>
+			<div className="child child2">child 2</div>
+			<div className="child child3">child 3</div>
+			<div className="child child4">child 4</div>
+			<div className="child child5">child 5</div>
+			<div className="child child6">child 6</div>
+			<div className="child child7">child 7</div>
+			<div className="child child8">child 8</div>
+			<div className="child child9">child 9</div>
 		</div>
 	)
 }
