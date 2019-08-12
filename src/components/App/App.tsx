d2e3f854022cac46a28aad0bd50ee8182f85e6e5
Wolven531 <anthony.williams.cs@gmail.com>
2019-08-11 22:40:48 -0600

    Use Grid in App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import logo from '../../logo.svg'
+import React, { FC } from 'react'
+import { Grid } from '../Grid/Grid'
 
+import logo from '../../logo.svg'
 import './App.css'
 
-const App: React.FC = () => {
+const App: FC = () => {
 	return (
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
 			</header>
+			<article>
+				<Grid />
+			</article>
 		</div>
 	)
 }
